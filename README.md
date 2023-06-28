475856e98ce731c81d3201ebd54567407e7e481c
seoungeunLee <118154731+seoungeunLee@users.noreply.github.com>
2023-06-28 14:29:48 +0900

    paw,rest 초기셋팅 및 버셀 연동

--- README.md
+++
@@ -1,1 +0,0 @@
-# Faw-rest
---
+++ faw-rest/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+ return (
+  <div className="text-[#333] bg-white text-center flex justify-center items-center w-full h-[100vh]">
+   Faw,rest 홈페이지가 만들어질 페이지 입니다.
+  </div>
+ );
+}
